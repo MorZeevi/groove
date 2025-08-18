a16aa51aea374b043530eb27fd1cebe018c59145
Mor <morzavi1@gmail.com>
2025-08-18 19:59:27 +0300

    fix

--- src/sections/header/HeaderNavigation.jsx
+++ src/sections/header/HeaderNavigation.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import './Header.css';
+import "./Header.css";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
