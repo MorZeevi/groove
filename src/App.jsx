639e5faeb05534ddc71e230e663a8f97f3580a82
Mor <morzavi1@gmail.com>
2025-08-19 14:13:04 +0300

    whathaa

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import Gallery from './sections/gallery/Gallery';
 import BestSeller from './sections/best-seller/BestSeller';
 import Insta from './sections/insta/Insta';
 import WorkWithUs from './sections/workWithUs/WorkWithUs';
+import WorkWithUsMobile from './sections/workWithUsMobile/WorkWithUs';
 import Locations from './sections/locations/locations';
 import Footer from './sections/footer/Footer';
+
+import { useResponsive, } from '../src/hooks/useResponsive';
 import './App.css'
 
 
 function App() {
+
+  const { isMobile } = useResponsive();
 
   return (
     <>
